c49315f2011418ae3ae7bb59a0207d07104afe2f
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-11-02 20:55:49 -0300

    GET / POST

--- markdown/fetchApi.md
+++ markdown/fetchApi.md
@@ -2,7 +2,7 @@
 
 > FAZENDO REQUISIÇÃOES HTTP COM JAVASCRIPT
 
-01
+01 - Retornando os resultados - GET
 ```js
 function getPosts() {
     fetch(`https://jsonplaceholder.typicode.com/posts/1`)
@@ -16,7 +16,7 @@ getPosts();
 
 ```
 
-02
+02 - Retornando os resultados - GET
 ```js
 // promise - async/await?
 async function getPosts(id) {
@@ -34,4 +34,45 @@ async function getPosts(id) {
 }());
 
 // retorno -> {userId: 1, id: 1, title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', body: 'quia et suscipit\nsuscipit recusandae consequuntur …strum rerum est autem sunt rem eveniet architecto'}
+```
+
+03 - Enviando informações - POST
+
+```js
+
+
+// POST
+async function addPost(data) {
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json; chatset=UTF-8'
+        } 
+    }
+
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/`,
+        options
+    );
+
+    if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`)
+    }
+
+    const json = await response.json();
+    return json;
+}
+
+
+(async function () { // IIFE fuction expression
+    const data = {
+        // userID: 1,
+        title: 'meu post',
+        body: 'Meu conteudo - Felipe'
+    }
+    const json = await addPost(data);
+    console.log(json)
+}());
+
 ```
--- markdown/fetchApi/http.js
+++ markdown/fetchApi/http.js
@@ -1,18 +1,56 @@
-const baseUrl = 'https://jsonplaceholder.typicode.com/';
-
-const uri = 'posts/1'
-
-
+// GET - recuperando as informações
+//==========================================================================/
+//==========================================================================/
 async function getPosts(id) {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    if (!response.ok) {
-        throw new Error(`${response.status} - ${response.statusText}`)
-    }
-    const json = await response.json();
-    return json;
+    return getResponse(response);
 }
 
 (async function () { // IIFE fuction expression
     const json = await getPosts(1);
     console.log(json)
 }());
+
+
+//==========================================================================/
+//==========================================================================/
+
+async function getResponse(response) {
+    if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`)
+    }
+
+    return await response.json();
+}
+
+// POST
+async function addPost(data) {
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json; chatset=UTF-8'
+        } 
+    }
+
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/`,
+        options
+    );
+
+    return getResponse(response);
+}
+
+
+(async function () { // IIFE fuction expression
+    const data = {
+        // userID: 1,
+        title: 'meu post',
+        body: 'Meu conteudo - Felipe'
+    }
+    const json = await addPost(data);
+    console.log(json)
+}());
+
+ 
+
